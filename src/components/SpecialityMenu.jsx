62472d3562432d99b17422ae2aae3d1875930664
Joseph Fabo <40829366+josephfabo@users.noreply.github.com>
2025-11-29 08:30:54 +0100

    Update SpecialityMenu.jsx

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -4,7 +4,17 @@ import {Link} from 'react-router-dom'
 
 const SpecialityMenu = () => {
   return (
-    <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+    <div
+  className="flex flex-col items-center gap-4 py-16 text-gray-800"
+  id="speciality"
+>
+  <h1 className="text-3xl font-medium">Find by Speciality</h1>
+  <p className="sm:w-1/3 text-center text-sm">
+    Simply browse through our extensive list of trusted doctors, schedule your
+    appointment
+  </p>
+
+  <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
     {specialityData.map((item, index) => (
       <Link
         onClick={() => scrollTo(0, 0)}
@@ -18,6 +28,7 @@ const SpecialityMenu = () => {
     ))}
   </div>
 </div>
+
   )
 }
 
